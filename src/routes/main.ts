28d282061021c1cb952d9ca18d43b0484158b124
dennyazevedofh <denny.filho@outlook.com>
2026-05-11 08:34:21 -0300

    Criando varios users

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma'
-import { createUser } from '../services/user';
+import { createUser, createUsers } from '../services/user';
 
 export const mainRouter = Router();
 
@@ -17,5 +17,19 @@ mainRouter.post('/user', async (req, res) => {
         res.status(201).json({ user });
     } else {
         res.status(400).json({ error: 'Email already exists' });
+    }
+})
+
+mainRouter.post('/users', async (req, res) => {
+    const result = await createUsers([
+        { name: 'Alice Smith', email: 'alice.smith@exemple.com' },
+        { name: 'Bob Johnson', email: 'bob.johnson@exemple.com' },
+        { name: 'Charlie Brown', email: 'charlie.brown@exemple.com' },
+        { name: 'David Wilson', email: 'david.wilson@exemple.com' }
+    ])
+    if (result) {
+        res.status(201).json({ ok: true })
+    } else {
+        res.status(400).json({ error: 'Error creating users' })
     }
 })
